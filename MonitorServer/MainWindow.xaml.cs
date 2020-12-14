06eeacf75f5ee75214caf5bd870a42930461adb8
Zachary D. Skelton <zbert@hotmail.com>
2020-12-14 00:47:00 -0600

    Outputs CPU to screen.

--- MonitorServer/MainWindow.xaml.cs
+++ MonitorServer/MainWindow.xaml.cs
@@ -12,21 +12,49 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Timers;
+using System.Windows.Threading;
 
 namespace MonitorServer
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Main Window for Performance Counter.
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Class Variables
         private bool _started = false;
-
+        PerformanceCounter cpuCounter;
+        PerformanceCounter ramCounter;
+        private static System.Windows.Threading.DispatcherTimer counter;
+        
+        // Init
         public MainWindow()
         { 
             InitializeComponent();
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+            counter = new System.Windows.Threading.DispatcherTimer();
+            counter.Tick += new EventHandler(OnTickEvent);
+            counter.Interval = new TimeSpan(0, 0, 1);
+            counter.Start();
         }
 
+        // Run on Tick
+        private void OnTickEvent(object sender, EventArgs e)
+        {
+            // Get Value of CPU
+            float cpuVal = cpuCounter.NextValue();
+            lbl_cpu.Content = String.Format("{0:0.00}%", cpuVal);
+            bar_cpu.Value = cpuVal;
+
+            float ramVal = ramCounter.NextValue();
+            Console.WriteLine(ramVal.ToString());            
+        }
+
+        // Button Click
         private void btn_monitor_click(object sender, RoutedEventArgs e)
         {
             _started = !_started;
